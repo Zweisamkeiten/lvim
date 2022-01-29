1ff37e305ec7f9e9c3541cd705ee952e7cd19478
einsam <snitxmhm@gmail.com>
2022-01-29 17:34:20 +0800

    insert mode c-f c-b as right and left

--- config.lua
+++ config.lua
@@ -22,23 +22,37 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- edit a default keymapping
 -- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
 
+-- own insert_mode keymapping
+lvim.keys.insert_mode["<C-f>"] = "<Right>"
+lvim.keys.insert_mode["<C-b>"] = "<Left>"
+
+-- Change cmp navigation to use n and p for navigation and j and k for doc page scroll
+local _, cmp = pcall(require, "cmp")
+lvim.builtin.cmp.mapping= {
+      ["<C-n>"] = cmp.mapping.select_prev_item(),
+      ["<C-p>"] = cmp.mapping.select_next_item(),
+      ["<C-k>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-j>"] = cmp.mapping.scroll_docs(4),
+}
+
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
--- local _, actions = pcall(require, "telescope.actions")
--- lvim.builtin.telescope.defaults.mappings = {
---   -- for input mode
---   i = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---     ["<C-n>"] = actions.cycle_history_next,
---     ["<C-p>"] = actions.cycle_history_prev,
---   },
---   -- for normal mode
---   n = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---   },
--- }
+local _, actions = pcall(require, "telescope.actions")
+lvim.builtin.telescope.defaults.mappings = {
+  -- for input mode
+  i = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+    ["<C-n>"] = actions.cycle_history_next,
+    ["<C-p>"] = actions.cycle_history_prev,
+  },
+  -- for normal mode
+  n = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+  },
+}
+
 
 -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
@@ -146,6 +160,7 @@ lvim.plugins = {
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 lvim.autocommands.custom_groups = {
-  { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
-  { "BufReadPost", "*", 'if line("\'\\"") > 1 && line("\'\\"") <= line("$") | execute "normal! g`\\"" | endif'},
+  -- { ""           , ""         , ""},
+  { "BufWinEnter", "*.lua"    , "setlocal ts=2 sw=2" },
+  { "BufReadPost", "*"        , 'if line("\'\\"") > 1 && line("\'\\"") <= line("$") | execute "normal! g`\\"" | endif'},
 }

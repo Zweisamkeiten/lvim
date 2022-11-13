7911b0a34cef5790e05718384374a3021c1e203a
Einsam <snitxmhm@gmail.com>
2022-11-13 10:30:13 +0800

    update: auto-save.nvim

--- config.lua
+++ config.lua
@@ -11,7 +11,7 @@ an executable
 -- general
 vim.opt.timeoutlen = 200
 lvim.log.level = "warn"
-lvim.format_on_save = true
+lvim.format_on_save = false
 lvim.builtin.which_key.setup.plugins.marks = true
 -- lvim.colorscheme = "tokyonight"
 lvim.transparent_window = true
@@ -227,6 +227,44 @@ lvim.plugins = {
     --  vim.o.timeoutlen = 500
     -- end
   },
+  {
+    "Pocco81/auto-save.nvim",
+    config = function()
+      require("auto-save").setup {
+        enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+        execution_message = {
+          message = function() -- message to print on save
+            return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+          end,
+          dim = 0.18, -- dim the color of `message`
+          cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+        },
+        trigger_events = { "InsertLeave", "TextChanged" }, -- vim events that trigger auto-save. See :h events
+        -- function that determines whether to save the current buffer or not
+        -- return true: if buffer is ok to be saved
+        -- return false: if it's not ok to be saved
+        condition = function(buf)
+          local fn = vim.fn
+          local utils = require("auto-save.utils.data")
+
+          if fn.getbufvar(buf, "&modifiable") == 1 and
+              utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+            return true -- met condition(s), can save
+          end
+          return false -- can't save
+        end,
+        write_all_buffers = false, -- write all buffers when the current one meets `condition`
+        debounce_delay = 135, -- saves the file at most every `debounce_delay` milliseconds
+        callbacks = { -- functions to be executed at different intervals
+          enabling = nil, -- ran when enabling auto-save
+          disabling = nil, -- ran when disabling auto-save
+          before_asserting_save = nil, -- ran before checking `condition`
+          before_saving = nil, -- ran before doing the actual save
+          after_saving = nil -- ran after doing the actual save
+        }
+      }
+    end,
+  },
 
 }
 
@@ -255,4 +293,18 @@ vim.opt.wrap = true
 require('luasnip').filetype_extend("javascriptreact", { "html" })
 -- vim.keymap.set('n', '+', '<Plug>(wildfire-fuel)')
 -- vim.keymap.set('v', '-', '<Plug>(wildfire-water)')
+vim.api.nvim_set_keymap("n", "<leader>n", ":ASToggle<CR>", {})
 local functions = require("user.functions")
+
+-- vim.g.clipboard = {
+--   name = "myClipboard",
+--   copy = {
+--     ["+"] = "xclip -sel clip -i",
+--     ["*"] = "xclip -sel clip -i",
+--   },
+--   paste = {
+--     ["+"] = "xclip -sel clip -o",
+--     ["*"] = "xclip -sel clip -o",
+--   },
+--   cache_enabled = 1,
+-- }

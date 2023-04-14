1148fc7df2e277a7b59ae22524864c5f992b56f5
Einsam <snitxmhm@gmail.com>
2023-04-14 14:16:42 +0800

    update: lvim stylua formate && support rust

--- config.lua
+++ config.lua
@@ -13,7 +13,7 @@ vim.opt.timeoutlen = 200
 lvim.log.level = "warn"
 lvim.format_on_save = false
 lvim.builtin.which_key.setup.plugins.marks = true
-lvim.colorscheme = "tokyonight-moon"
+lvim.colorscheme = "tokyonight-night"
 lvim.transparent_window = true
 vim.opt.relativenumber = true
 -- to disable icons and use a minimalist setup, uncomment the following
@@ -23,21 +23,22 @@ vim.opt.relativenumber = true
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode = {
-  ["<C-s>"] = ":w<cr>",
-  ["<S-l>"] = ":BufferLineCycleNext<CR>",
-  ["<S-h>"] = ":BufferLineCyclePrev<CR>",
+	["<C-s>"] = ":w<cr>",
+	["<S-l>"] = ":BufferLineCycleNext<CR>",
+	["<S-h>"] = ":BufferLineCyclePrev<CR>",
+	["<C-q>"] = ":q<cr>",
 }
 lvim.keys.term_mode = {
-  -- ["<ESC>"] = "<C-\\><C-N>",
-  ["<C-o>"] = "<C-\\><C-N>",
-  ["<C-h>"] = "<BACKSPACE>",
+	-- ["<ESC>"] = "<C-\\><C-N>",
+	["<C-o>"] = "<C-\\><C-N>",
+	["<C-h>"] = "<BACKSPACE>",
 }
 
 lvim.builtin.which_key.mappings["l"]["f"] = {
-  function()
-    require("lvim.lsp.utils").format { timeout_ms = 2000 }
-  end,
-  "Format",
+	function()
+		require("lvim.lsp.utils").format({ timeout_ms = 2000 })
+	end,
+	"Format",
 }
 -- unmap a default keymapping
 -- vim.keymap.del("n", "<C-Up>")
@@ -65,14 +66,14 @@ lvim.builtin.which_key.mappings["l"]["f"] = {
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 lvim.builtin.which_key.mappings["t"] = {
-  name = "Diagnostics",
-  b = { "<cmd>TroubleToggle<cr>", "trouble" },
-  w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "workspace" },
-  d = { "<cmd>TroubleToggle document_diagnostics<cr>", "document" },
-  q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
-  l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
-  r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
-  t = { "<cmd>ToggleTerm direction=horizontal size=12<cr>", "Terminal" }
+	name = "Diagnostics",
+	b = { "<cmd>TroubleToggle<cr>", "trouble" },
+	w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "workspace" },
+	d = { "<cmd>TroubleToggle document_diagnostics<cr>", "document" },
+	q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
+	l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
+	r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
+	t = { "<cmd>ToggleTerm direction=horizontal size=12<cr>", "Terminal" },
 }
 
 -- TODO: User Config for predefined plugins
@@ -85,18 +86,18 @@ lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "java",
-  "yaml",
+	"bash",
+	"c",
+	"javascript",
+	"json",
+	"lua",
+	"python",
+	"typescript",
+	"tsx",
+	"css",
+	"rust",
+	"java",
+	"yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -122,7 +123,7 @@ lvim.builtin.treesitter.highlight.enabled = true
 lvim.lsp.installer.setup.automatic_installation = false
 -- lvim.lsp.installer.setup.automatic_installation.exclude = {"asm_lsp"}
 -- lvim.lsp.installer.setup.ensure_installed = {"pyright", "jsonls", "yamlls", "bashls"}
--- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "asm_lsp" })
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "asm_lsp", "rust_analyzer" })
 
 -- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
 -- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
@@ -147,128 +148,259 @@ lvim.lsp.installer.setup.automatic_installation = false
 -- end
 
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  { command = "black", filetypes = { "python" } },
-  { command = "isort", filetypes = { "python" } },
-  {
-    -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-    command = "prettier",
-    ---@usage arguments to pass to the formatter
-    -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-    extra_args = { "--print-with", "100" },
-    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-    filetypes = { "typescript", "typescriptreact", "css", "html", "javascript", "javascriptreact" },
-  },
-}
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+	{ command = "black", filetypes = { "python" } },
+	{ command = "isort", filetypes = { "python" } },
+	{
+		-- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+		command = "prettier",
+		---@usage arguments to pass to the formatter
+		-- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+		extra_args = { "--print-with", "100" },
+		---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+		filetypes = { "typescript", "typescriptreact", "css", "html", "javascript", "javascriptreact" },
+	},
+	{ command = "stylua", filetypes = { "lua" } },
+})
 
 -- -- set additional linters
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-  -- { command = "pylint", filetypes = { "python" } },
-  -- {
-  --   -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-  --   command = "shellcheck",
-  --   ---@usage arguments to pass to the formatter
-  --   -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-  --   extra_args = { "--severity", "warning" },
-  -- },
-  -- {
-  --   command = "codespell",
-  --   ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-  --   filetypes = { "javascript", "python" },
-  -- },
-  {
-    command = "eslint",
-    filetypes = { "javascript", "javascriptreact" }
-  }
+local linters = require("lvim.lsp.null-ls.linters")
+linters.setup({
+	-- { command = "pylint", filetypes = { "python" } },
+	-- {
+	--   -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+	--   command = "shellcheck",
+	--   ---@usage arguments to pass to the formatter
+	--   -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+	--   extra_args = { "--severity", "warning" },
+	-- },
+	-- {
+	--   command = "codespell",
+	--   ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+	--   filetypes = { "javascript", "python" },
+	-- },
+	{
+		command = "eslint",
+		filetypes = { "javascript", "javascriptreact" },
+	},
+})
+
+local mason_path = vim.fn.glob(vim.fn.stdpath("data") .. "/mason/")
+local codelldb_adapter = {
+	type = "server",
+	port = "${port}",
+	executable = {
+		command = mason_path .. "bin/codelldb",
+		args = { "--port", "${port}" },
+		-- On windows you may have to uncomment this:
+		-- detached = false,
+	},
+}
+pcall(function()
+	require("rust-tools").setup({
+		tools = {
+			executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
+			reload_workspace_from_cargo_toml = true,
+			runnables = {
+				use_telescope = true,
+			},
+			inlay_hints = {
+				auto = true,
+				only_current_line = false,
+				show_parameter_hints = false,
+				parameter_hints_prefix = "<-",
+				other_hints_prefix = "=>",
+				max_len_align = false,
+				max_len_align_padding = 1,
+				right_align = false,
+				right_align_padding = 7,
+				highlight = "Comment",
+			},
+			hover_actions = {
+				border = "rounded",
+			},
+			on_initialized = function()
+				vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
+					pattern = { "*.rs" },
+					callback = function()
+						local _, _ = pcall(vim.lsp.codelens.refresh)
+					end,
+				})
+			end,
+		},
+		dap = {
+			adapter = codelldb_adapter,
+		},
+		server = {
+			on_attach = function(client, bufnr)
+				require("lvim.lsp").common_on_attach(client, bufnr)
+				local rt = require("rust-tools")
+				vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+			end,
+
+			capabilities = require("lvim.lsp").common_capabilities(),
+			settings = {
+				["rust-analyzer"] = {
+					lens = {
+						enable = true,
+					},
+					checkOnSave = {
+						enable = true,
+						command = "clippy",
+					},
+				},
+			},
+		},
+	})
+end)
+
+lvim.builtin.dap.on_config_done = function(dap)
+	dap.adapters.codelldb = codelldb_adapter
+	dap.configurations.rust = {
+		{
+			name = "Launch file",
+			type = "codelldb",
+			request = "launch",
+			program = function()
+				return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+			end,
+			cwd = "${workspaceFolder}",
+			stopOnEntry = false,
+		},
+	}
+end
+
+vim.api.nvim_set_keymap("n", "<m-d>", "<cmd>RustOpenExternalDocs<Cr>", { noremap = true, silent = true })
+lvim.builtin.which_key.mappings["C"] = {
+	name = "Rust",
+	r = { "<cmd>RustRunnables<Cr>", "Runnables" },
+	t = { "<cmd>lua _CARGO_TEST()<cr>", "Cargo Test" },
+	m = { "<cmd>RustExpandMacro<Cr>", "Expand Macro" },
+	c = { "<cmd>RustOpenCargo<Cr>", "Open Cargo" },
+	p = { "<cmd>RustParentModule<Cr>", "Parent Module" },
+	d = { "<cmd>RustDebuggables<Cr>", "Debuggables" },
+	v = { "<cmd>RustViewCrateGraph<Cr>", "View Crate Graph" },
+	R = {
+		"<cmd>lua require('rust-tools/workspace_refresh')._reload_workspace_from_cargo_toml()<Cr>",
+		"Reload Workspace",
+	},
+	o = { "<cmd>RustOpenExternalDocs<Cr>", "Open External Docs" },
+	y = { "<cmd>lua require'crates'.open_repository()<cr>", "[crates] open repository" },
+	P = { "<cmd>lua require'crates'.show_popup()<cr>", "[crates] show popup" },
+	i = { "<cmd>lua require'crates'.show_crate_popup()<cr>", "[crates] show info" },
+	f = { "<cmd>lua require'crates'.show_features_popup()<cr>", "[crates] show features" },
+	D = { "<cmd>lua require'crates'.show_dependencies_popup()<cr>", "[crates] show dependencies" },
 }
 
 -- Additional Plugins
 lvim.plugins = {
-  {
-    "folke/trouble.nvim",
-    cmd = "TroubleToggle",
-    config = function()
-      require("trouble").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end
-  },
-  {
-    "norcalli/nvim-colorizer.lua",
-    config = function()
-      require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
-        RGB = true, -- #RGB hex codes
-        RRGGBB = true, -- #RRGGBB hex codes
-        RRGGBBAA = true, -- #RRGGBBAA hex codes
-        rgb_fn = true, -- CSS rgb() and rgba() functions
-        hsl_fn = true, -- CSS hsl() and hsla() functions
-        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-      })
-    end,
-  },
-  {
-    "gcmt/wildfire.vim",
-    config = function()
-      vim.keymap.set('n', '+', '<Plug>(wildfire-fuel)')
-      vim.keymap.set('v', '+', '<Plug>(wildfire-fuel)')
-      vim.keymap.set('v', '-', '<Plug>(wildfire-water)')
-      -- vim.cmd([[
-      -- map + <Plug>(wildfire-fuel)
-      -- map - <Plug>(wildfire-water)
-      -- ]])
-    end
-  },
-  {
-    "tpope/vim-surround",
-    -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
-    -- setup = function()
-    --  vim.o.timeoutlen = 500
-    -- end
-  },
-  {
-    "Pocco81/auto-save.nvim",
-    config = function()
-      require("auto-save").setup {
-        enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
-        execution_message = {
-          message = function() -- message to print on save
-            return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
-          end,
-          dim = 0.18, -- dim the color of `message`
-          cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
-        },
-        trigger_events = { "InsertLeave", "TextChanged" }, -- vim events that trigger auto-save. See :h events
-        -- function that determines whether to save the current buffer or not
-        -- return true: if buffer is ok to be saved
-        -- return false: if it's not ok to be saved
-        condition = function(buf)
-          local fn = vim.fn
-          local utils = require("auto-save.utils.data")
+	{
+		"folke/trouble.nvim",
+		cmd = "TroubleToggle",
+		config = function()
+			require("trouble").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	},
+	{
+		"olimorris/onedarkpro.nvim",
+	},
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
+				RGB = true, -- #RGB hex codes
+				RRGGBB = true, -- #RRGGBB hex codes
+				RRGGBBAA = true, -- #RRGGBBAA hex codes
+				rgb_fn = true, -- CSS rgb() and rgba() functions
+				hsl_fn = true, -- CSS hsl() and hsla() functions
+				css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+				css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+			})
+		end,
+	},
+	{
+		"gcmt/wildfire.vim",
+		config = function()
+			vim.keymap.set("n", "+", "<Plug>(wildfire-fuel)")
+			vim.keymap.set("v", "+", "<Plug>(wildfire-fuel)")
+			vim.keymap.set("v", "-", "<Plug>(wildfire-water)")
+			-- vim.cmd([[
+			-- map + <Plug>(wildfire-fuel)
+			-- map - <Plug>(wildfire-water)
+			-- ]])
+		end,
+	},
+	{
+		"tpope/vim-surround",
+		-- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
+		-- setup = function()
+		--  vim.o.timeoutlen = 500
+		-- end
+	},
+	{
+		"Pocco81/auto-save.nvim",
+		config = function()
+			require("auto-save").setup({
+				enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+				execution_message = {
+					message = function() -- message to print on save
+						return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+					end,
+					dim = 0.18, -- dim the color of `message`
+					cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+				},
+				trigger_events = { "InsertLeave", "TextChanged" }, -- vim events that trigger auto-save. See :h events
+				-- function that determines whether to save the current buffer or not
+				-- return true: if buffer is ok to be saved
+				-- return false: if it's not ok to be saved
+				condition = function(buf)
+					local fn = vim.fn
+					local utils = require("auto-save.utils.data")
 
-          if fn.getbufvar(buf, "&modifiable") == 1 and
-              utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
-            return true -- met condition(s), can save
-          end
-          return false -- can't save
-        end,
-        write_all_buffers = false, -- write all buffers when the current one meets `condition`
-        debounce_delay = 135, -- saves the file at most every `debounce_delay` milliseconds
-        callbacks = { -- functions to be executed at different intervals
-          enabling = nil, -- ran when enabling auto-save
-          disabling = nil, -- ran when disabling auto-save
-          before_asserting_save = nil, -- ran before checking `condition`
-          before_saving = nil, -- ran before doing the actual save
-          after_saving = nil -- ran after doing the actual save
-        }
-      }
-    end,
-  },
-
+					if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+						return true -- met condition(s), can save
+					end
+					return false -- can't save
+				end,
+				write_all_buffers = false, -- write all buffers when the current one meets `condition`
+				debounce_delay = 135, -- saves the file at most every `debounce_delay` milliseconds
+				callbacks = { -- functions to be executed at different intervals
+					enabling = nil, -- ran when enabling auto-save
+					disabling = nil, -- ran when disabling auto-save
+					before_asserting_save = nil, -- ran before checking `condition`
+					before_saving = nil, -- ran before doing the actual save
+					after_saving = nil, -- ran after doing the actual save
+				},
+			})
+		end,
+	},
+	"simrat39/rust-tools.nvim",
+	{
+		"saecki/crates.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("crates").setup({
+				null_ls = {
+					enabled = true,
+					name = "crates.nvim",
+				},
+				popup = {
+					border = "rounded",
+				},
+			})
+		end,
+	},
+	{
+		"j-hui/fidget.nvim",
+		config = function()
+			require("fidget").setup()
+		end,
+	},
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
@@ -286,14 +418,14 @@ lvim.plugins = {
 -- })
 
 lvim.builtin.terminal.execs = {
-  { "lazygit", "<leader>gg", "LazyGit", "float" },
-  { "gdb", "<leader>tg", "GNU Debugger", "float" },
+	{ "lazygit", "<leader>gg", "LazyGit", "float" },
+	{ "gdb", "<leader>tg", "GNU Debugger", "float" },
 }
 
 vim.opt.wrap = true
 -- vim.opt.foldmethod = "expr"
 -- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-require('luasnip').filetype_extend("javascriptreact", { "html" })
+require("luasnip").filetype_extend("javascriptreact", { "html" })
 -- vim.keymap.set('n', '+', '<Plug>(wildfire-fuel)')
 -- vim.keymap.set('v', '-', '<Plug>(wildfire-water)')
 vim.api.nvim_set_keymap("n", "<leader>n", ":ASToggle<CR>", {})
@@ -324,4 +456,4 @@ vim.opt.foldenable = false
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
-lvim.builtin.gitsigns.active = true;
+lvim.builtin.gitsigns.active = true
